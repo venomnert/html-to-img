c7a00926f8cdb6f826bfc7f4d6030b731db9c68d
venomnert <neerthigan@gmail.com>
2020-02-12 13:05:59 -0500

    Added test html

--- lib/html_img_api/engine.ex
+++ lib/html_img_api/engine.ex
@@ -8,7 +8,7 @@ defmodule HtmlImgApi.Engine do
     @tmp_html
     |> File.write!(html, [:write])
     |> case do
-      :ok -> Porcelain.shell("wkhtmltoimage #{@tmp_html} #{@root}/#{DateTime.utc_now() |> DateTime.to_unix()}.png")
+      :ok -> Porcelain.shell("wkhtmltoimage #{"<h1>hello</h1>"} #{@root}/#{DateTime.utc_now() |> DateTime.to_unix()}.png")
       _ -> {:error, "error creating tmp html file"}
     end
 
